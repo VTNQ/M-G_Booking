278c9c5ae3a1ad543a70f481ccadc7a804015539
tranp6648 <tranp6648@gmail.com>
2024-12-12 11:11:16 +0700

    Update: AccountRepository.java

--- Web/src/main/java/com/vtnq/web/Repositories/AccountRepository.java
+++ Web/src/main/java/com/vtnq/web/Repositories/AccountRepository.java
@@ -2,6 +2,12 @@ package com.vtnq.web.Repositories;
 
 import com.vtnq.web.Entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    @Query(value = "FROM Account a WHERE a.email = :email")
+    Optional<Account> findByEmail(@Param("email") String email);
 }
